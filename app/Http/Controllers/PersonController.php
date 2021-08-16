a5e143cf28ac7ae056e090cfd4020216159cef9d
Carlos Wagner <wagner30023@gmail.com>
2021-08-16 02:14:18 -0300

    refatorando o Controller

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -17,7 +17,8 @@ class PersonController extends Controller
     public function all(){
         $person = person::all();
 
-        foreach($person as $persons){
+        foreach($person as $persons)
+        {
             $this->array['result'] [] = [
                 'id'        => $persons->id,
                 'firstname' => $persons->firstname,
@@ -33,7 +34,8 @@ class PersonController extends Controller
         // $person = person::find($id);
         $person = person::where('id', $id)->first();
 
-        if(!$person){
+        if(!$person)
+        {
             $this->array['error'] = 'ID nao localizado';
         }
 
@@ -44,45 +46,45 @@ class PersonController extends Controller
 
     public function new(Request $request)
     {
-        try{
-            $firstname = $request->input('firstname');
-            $lastname = $request->input('lastname');
-            $firstparent_id = $request->input('firstparent_id');
-            $secondparent_id = $request->input('secondparent_id');
+            try{
+                $firstname = $request->input('firstname');
+                $lastname = $request->input('lastname');
+                $firstparent_id = $request->input('firstparent_id');
+                $secondparent_id = $request->input('secondparent_id');
             
-            if(!$firstname && $lastname && $firstparent_id && $secondparent_id){
-                throw new exception();
+                if(!$firstname && $lastname && $firstparent_id && $secondparent_id)
+                {
+                    throw new exception();
+                }
+
+                $person = new person();
+                $person->firstname = $firstname;
+                $person->lastname = $lastname;
+                $person->firstparent_id = $firstparent_id;
+                $person->secondparent_id = $secondparent_id;
+                $person->save();
+
+                $this->array['result'] = [
+                'id' => $person->id,
+                'firstname' => $firstname,
+                'lastname' => $lastname
+            ];
+        
+            $this->array['result'] = 'Adicionado com successo';
+            return $this->array;
+        
+            } catch (Exception $e){
+                $this->array['error'] = 'campos nao adicionados';
+                $e->getmessage();
             }
-
-            $person = new person();
-        
-            $person->firstname = $firstname;
-            $person->lastname = $lastname;
-            $person->firstparent_id = $firstparent_id;
-            $person->secondparent_id = $secondparent_id;
-            $person->save();
-
-            $this->array['result'] = [
-            'id' => $person->id,
-            'firstname' => $firstname,
-            'lastname' => $lastname
-        ];
-        
-        $this->array['result'] = 'Adicionado com successo';
-
-        return $this->array;
-        }catch(Exception $e){
-            $this->array['error'] = 'campos nao adicionados';
-            $e->getmessage();
-        }
     }
 
     public function edit(Request $request,$id){
         $firstname = $request->input('firstname');
         $lastname = $request->input('lastname');
 
-
-        if(!$firstname && $lastname){
+        if(!$firstname && $lastname)
+        {
             $this->array['error'] = 'Campos nao atualizados';
         }
         
@@ -98,7 +100,8 @@ class PersonController extends Controller
     public function delete($id){
         $person = person::find($id);
 
-        if(!$person){
+        if(!$person)
+        {
             $this->array['error'] = 'ID nao existe'; 
         }
 
